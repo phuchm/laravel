c6700f1a50cc9290bee1672528755846ed741677
Hoang Minh Phuc <phuc.hoangminh@toshiba-tsdv.com>
2017-03-06 11:45:06 +0700

    Add multiple languages for login and sigup GUI

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,33 @@
 <div id="loginbox" style="margin-top:50px; width: 30%; margin-left: 35%;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
     <div class="panel panel-info" >
         <div class="panel-heading">
-            <div class="panel-title">Sign In</div>
-            <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
+            <div class="panel-title">{{ trans('content.SignIn') }}</div>
+            <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">{{ trans('content.Forgot password?') }}</a></div>
         </div>
 
         <div style="padding-top:30px" class="panel-body" >
             <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
 
             <form id="loginform" class="form-horizontal" role="form">
+                <div id="signupalert" style="display:none" class="alert alert-danger">
+                    <p>{{ trans('content.Error') }}:</p>
+                    <span></span>
+                </div>
+
                 <div style="margin-bottom: 25px" class="input-group">
                     <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                    <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email">
+                    <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="{{ trans('content.Username or email') }}">
                 </div>
     
                 <div style="margin-bottom: 25px" class="input-group">
                     <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                    <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                    <input id="login-password" type="password" class="form-control" name="password" placeholder="{{ trans('content.Password') }}">
                 </div>
     
                 <div class="input-group">
                     <div class="checkbox">
                         <label>
-                            <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
+                            <input id="login-remember" type="checkbox" name="remember" value="1"> {{ trans('content.Remember me') }}
                         </label>
                     </div>
                 </div>
@@ -30,17 +35,17 @@
                 <div style="margin-top:10px" class="form-group">
                     <!-- Button -->
                     <div class="col-sm-12 controls">
-                        <a id="btn-login" href="#" class="btn btn-success">Login  </a>
-                        <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
+                        <a id="btn-login" href="#" class="btn btn-success">{{ trans('content.Login') }}</a>
+                        <a id="btn-fblogin" href="#" class="btn btn-primary">{{ trans('content.Login with Facebook') }}</a>
                     </div>
                 </div>
     
                 <div class="form-group">
                     <div class="col-md-12 control">
                         <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-                            Don't have an account!
+                            {{ trans('content.Don\'t have an account!') }}
                             <a href="" ng-click="signUp()">
-                            Sign Up Here
+                            {{ trans('content.Sign Up here') }}
                             </a>
                         </div>
                     </div>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,46 +1,46 @@
 <div id="signupbox" style="margin-top:50px; width: 30%; margin-left: 35%;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
     <div class="panel panel-info">
         <div class="panel-heading">
-            <div class="panel-title">Sign Up</div>
+            <div class="panel-title">{{ trans('content.SignUp') }}</div>
             <div style="float:right; font-size: 85%; position: relative; top:-10px">
-                <a id="signinlink" href="" ng-click="logIn()">Sign In</a>
+                <a id="signinlink" href="" ng-click="logIn()">{{ trans('content.SignIn') }}</a>
             </div>
         </div>
         <div class="panel-body" >
             <form id="signupform" class="form-horizontal" role="form">
                 <div id="signupalert" style="display:none" class="alert alert-danger">
-                    <p>Error:</p>
+                    <p>{{ trans('content.Error') }}:</p>
                     <span></span>
                 </div>
     
                 <div class="form-group">
-                    <label for="email" class="col-md-3 control-label">Email</label>
+                    <label for="email" class="col-md-3 control-label">{{ trans('content.Email') }}</label>
                     <div class="col-md-9">
-                        <input type="text" class="form-control" name="email" placeholder="Email Address">
+                        <input type="text" class="form-control" name="email" placeholder="{{ trans('content.Email Address') }}">
                     </div>
                 </div>
     
                 <div class="form-group">
-                    <label for="firstname" class="col-md-3 control-label">First Name</label>
+                    <label for="firstname" class="col-md-3 control-label">{{ trans('content.First Name') }}</label>
                     <div class="col-md-9">
-                        <input type="text" class="form-control" name="firstname" placeholder="First Name">
+                        <input type="text" class="form-control" name="firstname" placeholder="{{ trans('content.First Name') }}">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="lastname" class="col-md-3 control-label">Last Name</label>
+                    <label for="lastname" class="col-md-3 control-label">{{ trans('content.Last Name') }}</label>
                     <div class="col-md-9">
-                        <input type="text" class="form-control" name="lastname" placeholder="Last Name">
+                        <input type="text" class="form-control" name="lastname" placeholder="{{ trans('content.Last Name') }}">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="password" class="col-md-3 control-label">Password</label>
+                    <label for="password" class="col-md-3 control-label">{{ trans('content.Password') }}</label>
                     <div class="col-md-9">
-                        <input type="password" class="form-control" name="passwd" placeholder="Password">
+                        <input type="password" class="form-control" name="passwd" placeholder="{{ trans('content.Password') }}">
                     </div>
                 </div>
     
                 <div class="form-group">
-                    <label for="icode" class="col-md-3 control-label">Invitation Code</label>
+                    <label for="icode" class="col-md-3 control-label">{{ trans('content.Invitation Code') }}</label>
                     <div class="col-md-9">
                         <input type="text" class="form-control" name="icode" placeholder="">
                     </div>
@@ -49,14 +49,14 @@
                 <div class="form-group">
                     <!-- Button -->
                     <div class="col-md-offset-3 col-md-9">
-                        <button id="btn-signup" type="button" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp; Sign Up</button>
-                        <span style="margin-left:8px;">or</span>
+                        <button id="btn-signup" type="button" class="btn btn-info"><i class="icon-hand-right"></i> &nbsp; {{ trans('content.SignUp') }}</button>
+                        <span style="margin-left:8px;">{{ trans('content.Or') }}</span>
                     </div>
                 </div>
     
                 <div style="border-top: 1px solid #999; padding-top:20px"  class="form-group">
                     <div class="col-md-offset-3 col-md-9">
-                        <button id="btn-fbsignup" type="button" class="btn btn-primary"><i class="icon-facebook"></i>   Sign Up with Facebook</button>
+                        <button id="btn-fbsignup" type="button" class="btn btn-primary"><i class="icon-facebook"></i>   {{ trans('content.Sign Up with Facebook') }}</button>
                     </div>
                 </div>
             </form>
